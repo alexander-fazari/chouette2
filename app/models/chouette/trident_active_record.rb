5c4250ea687500eaada00675a9ecf38c3a5ae8b2
vincent <vincent.paturet@entur.org>
2019-05-07 11:06:39 +0200

    ROR-621 Add a timestamp for dataset update in NeTEx export

--- app/models/chouette/trident_active_record.rb
+++ app/models/chouette/trident_active_record.rb
@@ -88,7 +88,7 @@ class Chouette::TridentActiveRecord < Chouette::ActiveRecord
     # Update the referential last update timestamp
     # The timestamp is based on the client (Chouette2) timezone
     def update_referential_last_update_timestamp
-        #logger.info 'Update referential last update timestamp'
+        logger.info 'Update referential last update timestamp'
         begin
           connection = ActiveRecord::Base.connection.raw_connection
           connection.prepare("last_update_timestamp","UPDATE referential_last_update SET last_update_timestamp=$1")
@@ -98,6 +98,9 @@ class Chouette::TridentActiveRecord < Chouette::ActiveRecord
         rescue => exception
           puts 'Could not update referential last update timestamp'
           puts exception.backtrace
+          logger.warn 'Could not update referential last update timestamp'
+          logger.warn exception.backtrace
+
         end
     end
 
